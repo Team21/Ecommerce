aceccbe3d5f49c2f708ccd30bf84d07b945c8da7
AndDeve <AndDeve@AndDeve>
2016-03-14 17:23:43 +0200

    add db class

--- src/java/dao/CategoryDAO.java
+++ src/java/dao/CategoryDAO.java
@@ -5,19 +5,48 @@
  */
 package dao;
 
+import factory.MysqlFactory;
 import interfaces.DAOInter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.RowSet;
+import pojo.Category;
+import pojo.Permission;
 
 /**
  *
  * @author Hossam
  */
 public class CategoryDAO implements DAOInter{
+    
+    Statement stmt;
+    ResultSet resultSet;
+    Connection connection;
+    String sql;
+    PreparedStatement ps;
 
     @Override
-    public int insertObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int insertObject(Object obj) {
+        Category c = (Category) obj;
+        int rows = 0;
+        try {
+            connection = MysqlFactory.getConnection();
+
+            sql = "insert into category (name) values(?) ";
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, c.getName());
+            //ps.setBytes(2, c.getImage());
+            rows = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PermissionDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rows;
     }
 
     @Override
@@ -26,8 +55,29 @@ public class CategoryDAO implements DAOInter{
     }
 
     @Override
-    public Object findObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Object findObject(Object obj) {
+        Category c = (Category) obj;
+        sql = "SELECT * FROM category WHERE name = ?";
+        try {
+            connection = MysqlFactory.getConnection();
+
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, c.getName());
+            resultSet = ps.executeQuery();
+            if (!resultSet.isBeforeFirst()) {
+                System.out.println("Permission Not Exist");
+                return null;
+            } else {
+                resultSet.next();
+                c = new Category();
+                c.setImage(resultSet.getBytes("image"));
+                c.setName(resultSet.getString("name"));
+                c.setId(resultSet.getInt("id"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return c;
     }
 
     @Override
--- src/java/dao/PageDAO.java
+++ src/java/dao/PageDAO.java
@@ -5,9 +5,19 @@
  */
 package dao;
 
+import factory.MysqlFactory;
 import interfaces.DAOInter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.RowSet;
+import pojo.Page;
+import pojo.Permission;
 
 
 /**
@@ -16,9 +26,27 @@ import javax.sql.RowSet;
  */
 public class PageDAO implements DAOInter{
 
+    Statement stmt;
+    ResultSet resultSet;
+    Connection connection;
+    String sql;
+    PreparedStatement ps;
+    
     @Override
-    public int insertObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int insertObject(Object obj) {
+        Page p = (Page) obj;
+        int rows = 0;
+        try {
+            connection = MysqlFactory.getConnection();
+
+            sql = "insert into page (name) values(?) ";
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, p.getName());
+            rows = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PermissionDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rows;
     }
 
     @Override
@@ -27,8 +55,28 @@ public class PageDAO implements DAOInter{
     }
 
     @Override
-    public Object findObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Object findObject(Object obj) {
+        Page p = (Page) obj;
+        sql = "SELECT * FROM page WHERE name = ?";
+        try {
+            connection = MysqlFactory.getConnection();
+
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, p.getName());
+            resultSet = ps.executeQuery();
+            if (!resultSet.isBeforeFirst()) {
+                System.out.println("Page Not Exist");
+                return null;
+            } else {
+                resultSet.next();
+                p = new Page();
+                p.setName(resultSet.getString("name"));
+                p.setId(resultSet.getInt("id"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return p;
     }
 
     @Override
