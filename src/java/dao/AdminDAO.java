c008639c4c3b3e451c8a45a7960ffe702d214f5b
mrnull <mrnull@blackhat>
2016-03-15 17:21:50 +0200

    update the structure

--- src/java/dao/AdminDAO.java
+++ src/java/dao/AdminDAO.java
@@ -5,46 +5,14 @@
  */
 package dao;
 
-import interfaces.DAOInter;
 import java.util.Collection;
 import javax.sql.RowSet;
+import pojo.User;
 
 /**
  *
  * @author Ahmad Moawad <ahmadmoawad3@gmail.com>
  */
-public class AdminDAO implements DAOInter{
+public class AdminDAO extends User{
 
-
-    @Override
-    public int insertObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean deleteObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Object findObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean updateObject(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public RowSet selectObjectsRS(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Collection selectObjectsTO(Object admin) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
 }
